2d89e5a4a09c38199a37d922d0952f356660a738
R. Tyler Julian <trjulian@gmail.com>
2015-03-12 12:15:59 -0700

    Initial scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name carefulHarmonicaApp
+ * @description
+ * # carefulHarmonicaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('carefulHarmonicaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
