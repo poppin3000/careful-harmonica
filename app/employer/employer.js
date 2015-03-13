cb4cbc34cac77d5ad2c855d6ba28604dfc164177
Ben Creasy <contact@bencreasy.com>
2015-03-13 14:58:38 -0700

    (feat) Employer view and skeleton controller

---
+++ app/employer/employer.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+  angular
+  .module('app.employer', [])
+  .controller('EmployerCtrl', EmployerCtrl)
+
+  function EmployerCtrl($scope) {
+    // Insert logic
+  }
+})();
