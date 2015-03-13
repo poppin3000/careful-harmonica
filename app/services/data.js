bc3dbbc2173ff937402536072c522db3c0b22867
Ben Creasy <contact@bencreasy.com>
2015-03-12 19:59:17 -0700

    Start mock of data but changing direction to manual entry on firebase.com

---
+++ app/services/data.js
@@ -0,0 +1,58 @@
+
+// Lookup for generating random data
+var mock_users = ['Ben', 'Pavan', 'Tyler', 'Ryan'];
+var mock_employers = ['Google', 'LinkedIn', 'Facebook', 'Twitter'];
+var mock_positions = ['Software Engineer', 'Software Developer', 'UX Engineer', 'Validation Engineer'];
+var mock_resume = 'Insert resume file';
+var mock_locations = ['San Jose', 'San Francisco', 'Seattle', 'Los Angeles', 'Portland'];
+var mock_experience = '6 months';
+var mock_education = ['Bachelor\'s degree', 'High-school diploma'];
+
+var users = [];
+mock_users.forEach({
+
+})
+/*
+{
+  "users": {
+    "[email address]": {
+      "name": "[name]"
+    }
+  }
+}
+
+users[email address]      
+  name      
+  resume      
+  cover letter      
+  desired job     
+  education     
+  experience      
+  location      
+  profile picture     
+  contact info      
+  badges      
+  friends     
+  to-dos      
+  employers     
+  employer name     
+    position    
+    job title   
+      created at  
+      description 
+      progress  
+        application (bool)
+        resume
+        research notes
+        emails
+        calls
+        interviews
+        responses
+
+badges[badge_name]
+  icon
+  point_property
+
+employers
+careers
+*/
