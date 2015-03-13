45063a88fe032d3dee61680a7ca694d12c4a3c10
R. Tyler Julian <trjulian@gmail.com>
2015-03-12 19:34:35 -0700

    (feat) Set up app ui routing for all views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,11 +8,121 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('carefulHarmonicaApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ]);
+
+// ********************** Route Definitions **********************
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider
+
+    // ********************** Dashboard **********************
+    .state('dashboard', {
+      url: '/',
+      views: {
+        '': {
+          templateUrl: 'app/views/dashboard/dashboard.html',
+          controller: 'DashboardCtrl'
+        },
+        'tasks': {
+          templateUrl: 'app/views/dashboard/tasks.html',
+          controller: 'TaskCtrl'
+        },
+        'score': {
+          templateUrl: 'app/views/dashboard/score.html',
+          controller: 'ScoreCtrl'
+        },
+        'analytics': {
+          templateUrl: 'app/views/dashboard/analytics.html',
+          controller: 'AnalyticsCtrl'
+        },
+        'achievements': {
+          templateUrl: 'app/views/dashboard/achievements.html',
+          controller: 'AchievementsCtrl'
+        },
+        'social': {
+          templateUrl: 'app/views/dashboard/social.html',
+          controller: 'SocialCtrl'
+        }
+      }
+    })
+
+    // ********************** User Profile **********************
+    .state('profile', {
+      url: '/profile',
+      views: {
+        '': {
+          templateUrl: 'app/views/profile/profile.html',
+          controller: 'ProfileCtrl'
+        }
+      }
+    })
+
+    // ********************** Job Page **********************
+    .state('job', {
+      url: '/job',
+      views: {
+        '': {
+          templateUrl: 'app/views/job/job.html',
+          controller: 'ProfileCtrl'
+        }
+      }
+    })
+
+    // ********************** Authentication **********************
+    .state('signup', {
+      url: '/signup',
+      views: {
+        '': {
+          templateUrl: 'app/views/auth/signup.html',
+          controller: 'SignupCtrl'
+        }
+      }
+    })
+
+    .state('signin', {
+      url: '/signin',
+      views: {
+        '': {
+          templateUrl: 'app/views/auth/signin.html',
+          controller: 'SigninCtrl'
+        }
+      }
+    })
+
+    // ********************** Onboarding **********************
+    .state('onboard', {
+      url: '/onboard',
+      views: {
+        '': {
+          templateUrl: 'app/views/onboard/onboard.html',
+          controller: 'OnboardCtrl'
+        },
+        'dream': {
+          templateUrl: 'app/views/onboard/dream.html',
+          controller: 'OnboardCtrl'
+        },
+        'upload': {
+          templateUrl: 'app/views/onboard/upload.html',
+          controller: 'OnboardCtrl'
+        },
+        'goal': {
+          templateUrl: 'app/views/onboard/goal.html',
+          controller: 'OnboardCtrl'
+        },
+        'install': {
+          templateUrl: 'app/views/onboardinstall.html',
+          controller: 'OnboardCtrl'
+        }
+      }
+    })
+});
