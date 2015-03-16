c10df8480664742f4fdbe6f808e1340253f7d259
R. Tyler Julian <trjulian@gmail.com>
2015-03-16 15:07:43 -0700

    (feat) Add achievements/task history to factories

--- app/services/factory.js
+++ app/services/factory.js
@@ -24,29 +24,22 @@
       };
 
       var getAchievements = function() {
-        
-        return [
-          {
-            title: 'Sent a resume',
-            description: 'Sent your resume to IBM and received 20 points!',
-            date: '3/13/15'
-          },
-          {
-            title: 'Researched a company',
-            description: 'Researched IBM and received 10 points!',
-            date: '3/13/15'
-          },
-          {
-            title: 'Got an interview!',
-            description: 'Had an in-person interview with Apple and received 100 points!',
-            date: '3/12/15'
-          },
-          {
-            title: 'Found a job posting',
-            description: 'Found a job posting by IBM and received 5 points!',
-            date: '3/10/15'
-          }
-        ];
+        var results = [];
+
+        for (var employer in Employers.data) {
+          var task = {};
+          var job = Employers.data[employer];
+
+          task.employer = employer;
+          task.type = Dictionary.findRecentTask(job);
+          task.title = Dictionary.taskTitle(task.type);
+          task.description = Dictionary.taskDescription(task.type)
+          task.score = Dictionary.taskScore(task.type);
+
+          results.push(task);
+        }
+
+        return results;
       };
 
       return {
