275d22376b41ff8dda5fa0fca2f6199bfa8d5c3b
Ryan Carey <me@ryancarey.ca>
2015-03-16 14:04:19 -0700

    use Compass for dist build instead of node-sass - better support on circle-CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -213,35 +213,30 @@ module.exports = function (grunt) {
       }
     },
 
-    // *** Replaced with grunt-sass
+    // Only runs for dist build
     // Compiles Sass to CSS and generates necessary files if requested
-    // compass: {
-    //   options: {
-    //     sassDir: '<%= yeoman.app %>/styles',
-    //     cssDir: '.tmp/styles',
-    //     generatedImagesDir: '.tmp/images/generated',
-    //     imagesDir: '<%= yeoman.app %>/images',
-    //     javascriptsDir: '<%= yeoman.app %>/scripts',
-    //     fontsDir: '<%= yeoman.app %>/styles/fonts',
-    //     importPath: './bower_components',
-    //     httpImagesPath: '/images',
-    //     httpGeneratedImagesPath: '/images/generated',
-    //     httpFontsPath: '/styles/fonts',
-    //     relativeAssets: false,
-    //     assetCacheBuster: false,
-    //     raw: 'Sass::Script::Number.precision = 10\n'
-    //   },
-    //   dist: {
-    //     options: {
-    //       generatedImagesDir: '<%= yeoman.dist %>/images/generated'
-    //     }
-    //   },
-    //   server: {
-    //     options: {
-    //       sourcemap: true
-    //     }
-    //   }
-    // },
+    compass: {
+      options: {
+        sassDir: '<%= yeoman.app %>/styles',
+        cssDir: '.tmp/styles',
+        generatedImagesDir: '.tmp/images/generated',
+        imagesDir: '<%= yeoman.app %>/images',
+        javascriptsDir: '<%= yeoman.app %>/scripts',
+        fontsDir: '<%= yeoman.app %>/styles/fonts',
+        importPath: './bower_components',
+        httpImagesPath: '/images',
+        httpGeneratedImagesPath: '/images/generated',
+        httpFontsPath: '/styles/fonts',
+        relativeAssets: false,
+        assetCacheBuster: false,
+        raw: 'Sass::Script::Number.precision = 10\n'
+      },
+      dist: {
+        options: {
+          generatedImagesDir: '<%= yeoman.dist %>/images/generated'
+        }
+      },
+    },
 
     sass: {
       files: {
@@ -438,7 +433,7 @@ module.exports = function (grunt) {
         'sass'
       ],
       dist: [
-        'sass',
+        'compass',
         'imagemin',
         'svgmin'
       ]
