592c0ab1b3cfc60e1e74152597cc7aac03469d59
R. Tyler Julian <trjulian@gmail.com>
2015-03-18 12:31:56 -0700

    (cleanup) Remove old data dictionary definitions

--- app/services/dictionary.js
+++ app/services/dictionary.js
@@ -89,54 +89,9 @@
         return dictionary[type];
       };
 
-      var taskTitle = function(type) {
-        var dictionary = {
-          'application': 'Gather ye\' papers!',
-          'resume': 'Who goes there?',
-          'researchNotes': 'Reconnaissance',
-          'emails': 'Message in a bottle',
-          'calls': 'Ahoy, matey!',
-          'interviews': 'All hands on deck!',
-          'followups': 'Don\'t forget'
-        };
-
-        return dictionary[type];
-      };
-
-      var taskDescription = function(type) {
-        var dictionary = {
-          'application': 'Send an application to',
-          'resume': 'Send a resume to',
-          'researchNotes': 'Research',
-          'emails': 'Send an email to',
-          'calls': 'Make a call to',
-          'interviews': 'Interview with',
-          'followups': 'Follow up with'
-        };
-
-        return dictionary[type];
-      };
-
-      var taskScore = function(type) {
-        var dictionary = {
-          'application': 20,
-          'resume': 10,
-          'researchNotes': 5,
-          'emails': 10,
-          'calls': 25,
-          'interviews': 50,
-          'followups': 30
-        };
-
-        return dictionary[type];
-      };
-
       return {
         findNextTask: findNextTask,
         findRecentTask: findRecentTask,
-        taskTitle: taskTitle,
-        taskDescription: taskDescription,
-        taskScore: taskScore,
         taskDetails: taskDetails
       };
     });
