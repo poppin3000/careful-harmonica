e9c1ec00ade0df7a5791c8999f3a890d8f1e2f87
Pavan Ravipati <poppin3000@gmail.com>
2015-03-18 14:09:43 -0700

    (test) Add sinon to karma test suite

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -4,52 +4,103 @@ var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
 
-describe('the friend module', function() {
 
-  describe('the friend module\'s html templates', function () {
+describe('the Dictionary factory', function() {
 
-    it('should not have any <html> tags', function () {
+  describe('the Dictionary\'s default functions', function() {
+    
+    var Dictionary;
+    beforeEach(module('app.dictionary'));
+    beforeEach(inject(function (_Dictionary_) {
+      Dictionary = _Dictionary_;
+    }));
 
-      //try using fs directly without using require statement
-
-
+    it('should have a findNextTask function', function() {
+      expect(Dictionary).to.have.property('findNextTask');
     });
 
-    // it('should have a section for badges', function () {
+    it('should have a findRecentTask function', function() {
+      expect(Dictionary).to.have.property('findRecentTask');
+    });
 
-    // });
+    it('should have a taskTitle function', function() {
+      expect(Dictionary).to.have.property('taskTitle');
+    });
 
-    // it('should have a section for to-do\'s', function () {
+    it('should have a taskDescription function', function() {
+      expect(Dictionary).to.have.property('taskDescription');
+    });
 
-    // });
+    it('should have a taskScore function', function() {
+      expect(Dictionary).to.have.property('taskScore');
+    });
 
-    // it('should have a section for recent activities', function () {
-
-    // });
+    it('should have a taskDetails function', function() {
+      expect(Dictionary).to.have.property('taskDetails');
+    });
 
   });
 
-
-
-
+  describe('', function(){})
 });
 
-describe('Controller: OnboardCtrl', function () {
 
-  // TODO: Implement controller tests 
+
+
+
+
+
+
+
+
+
+
+// describe('the friend module', function() {
+
+//   describe('the friend module\'s html templates', function () {
+
+//     it('should not have any <html> tags', function () {
+
+//       //try using fs directly without using require statement
+
+
+//     });
+
+//     // it('should have a section for badges', function () {
+
+//     // });
+
+//     // it('should have a section for to-do\'s', function () {
+
+//     // });
+
+//     // it('should have a section for recent activities', function () {
+
+//     // });
+
+//   });
+
+
+
+
+// });
+
+// describe('Controller: OnboardCtrl', function () {
+
+//   // TODO: Implement controller tests 
   
-  // load the controller's module
-  // beforeEach(module('carefulHarmonicaApp'));
+//   // load the controller's module
+//   // beforeEach(module('carefulHarmonicaApp'));
 
-  // var OnboardCtrl,
-  //   scope;
+//   // var OnboardCtrl,
+//   //   scope;
 
-  // Initialize the controller and a mock scope
-  // beforeEach(inject(function ($controller, $rootScope) {
-  //   scope = $rootScope.$new();
-  //   OnboardCtrl = $controller('OnboardCtrl', {
-  //     $scope: scope
-  //   });
-  // }));
+//   // Initialize the controller and a mock scope
+//   // beforeEach(inject(function ($controller, $rootScope) {
+//   //   scope = $rootScope.$new();
+//   //   OnboardCtrl = $controller('OnboardCtrl', {
+//   //     $scope: scope
+//   //   });
+//   // }));
   
-});
+// });
