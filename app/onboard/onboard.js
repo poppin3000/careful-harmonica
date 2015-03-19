3362cb891a28bbd4da810bdb767bf21bc93d6daf
Ben Creasy <contact@bencreasy.com>
2015-03-18 19:36:32 -0700

    (feat) Start OnboardCtrl

--- app/onboard/onboard.js
+++ app/onboard/onboard.js
@@ -2,16 +2,12 @@
   'use strict';
 
   angular.module('app.onboard', ['ngMaterial'])
-    .controller('OnboardCtrl', ['$scope', 'Data', function($scope, Data) {
+    .controller('OnboardCtrl', OnboardCtrl);
+      
+    OnboardCtrl.$inject = ['$scope'];
+    function OnboardCtrl($scope) {
       $scope.user = {};
-      $scope.signup = function() {
-        Data.signup($scope.user.email, $scope.user.password);
-      };
-
-      $scope.signin = function() {
-        Data.signin($scope.user.email, $scope.user.password);
-      };
-
-    }]);
+      
+    }
 
 })();
