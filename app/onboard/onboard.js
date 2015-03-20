cf7679fdb8e5f7220117c62aef4328137d430eca
Ben Creasy <contact@bencreasy.com>
2015-03-19 19:03:55 -0700

    (feat) Setup three-way data-binding for onboarding

--- app/onboard/onboard.js
+++ app/onboard/onboard.js
@@ -1,13 +1,20 @@
 (function() {
   'use strict';
 
-  angular.module('app.onboard', ['ngMaterial'])
+  angular.module('app.onboard', ['ngMaterial', 'firebase'])
     .controller('OnboardCtrl', OnboardCtrl);
       
-    OnboardCtrl.$inject = ['$scope'];
-    function OnboardCtrl($scope) {
+    OnboardCtrl.$inject = ['$scope', '$firebaseObject'];
+    function OnboardCtrl($scope, $firebaseObject) {
       $scope.user = {};
-      
+      var refURL = 'https://careful-harmonica.firebaseio.com/';
+      var ref = new Firebase(refURL);
+      ref.onAuth(function(authData) {
+        var user = ref.child('users').child(authData.uid);
+        var obj = $firebaseObject(user);
+        obj.$loaded().then(function(data) {
+          data.$bindTo($scope, 'user');
+        });
+      });
     }
-
 })();
